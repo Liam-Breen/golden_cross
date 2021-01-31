c3acb128d86abd409b92b7ce6fa0b752cc4df751
Liam-Breen <liam.breen25@gmail.com>
2021-01-31 15:44:10 +1100

    renaming

--- stock_market/stock_market/urls.py
+++ stock_market/stock_market/urls.py
@@ -1,4 +1,4 @@
-"""golden_cross URL Configuration
+"""stock_market URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
